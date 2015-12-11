fb58de648b2a3b8d1eb20ccd733f6456b190af74
trih <holetri@gmails.com>
2015-12-11 15:00:52 +0000

    add controlling motor

---
+++ was_motor/src/pcf8591.cpp
@@ -0,0 +1,50 @@
+#include "ros/ros.h"
+#include "std_msgs/UInt16MultiArray.h"
+#include "BlackLib/BlackLib.h"
+#include <sstream>
+
+#define SLOPE        1
+#define INTERCEPTER  0
+
+#define NUM_RANGE_SENSOR        5
+
+#define WAS_DEBUG
+
+//I2C2 accessing to PCF8591
+
+int main(int argc, char **argv)
+{
+        ros::init(argc, argv, "was_range_sensor");
+        ros::NodeHandle nh;
+        ros::Publisher range_pub = nh.advertise<std_msgs::UInt16MultiArray>("was_sensor/ultrasound", 1000);
+        ros::Rate loop_rate(10);
+
+        std_msgs::UInt16MultiArray was_range_msg;
+
+        BlackLib::BlackADC *readADCBuff[NUM_RANGE_SENSOR];
+	readADCBuff[0]= new BlackLib::BlackADC(BlackLib::AIN0);
+	readADCBuff[1]= new BlackLib::BlackADC(BlackLib::AIN1);
+	readADCBuff[2]= new BlackLib::BlackADC(BlackLib::AIN2);
+	readADCBuff[3]= new BlackLib::BlackADC(BlackLib::AIN3);
+	readADCBuff[4]= new BlackLib::BlackADC(BlackLib::AIN4);
+
+        while (ros::ok()) {
+        	was_range_msg.data.clear();
+        	for (int i = 0; i < NUM_RANGE_SENSOR; ++i)
+        	{
+                        was_range_msg.data.push_back( ( readADCBuff[i]->getNumericValue() * SLOPE ) + INTERCEPTER );
+
+#ifdef  WAS_DEBUG
+                        std::cout << "ADC[" << i << "]: " << readADCBuff[i]->getNumericValue() << " | ";
+#endif
+        	}
+                std::cout << std::endl;
+
+                range_pub.publish(was_range_msg);
+
+                ros::spinOnce();
+
+                loop_rate.sleep();
+        }
+        return 0;
+}
